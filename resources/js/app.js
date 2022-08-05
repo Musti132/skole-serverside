d5563ce574412957ecf9afa1934e98aa63066f6a
Mustafa al-nashie <44943708+Musti132@users.noreply.github.com>
2022-08-05 15:11:30 +0200

    Vue added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+loadFonts()
+
+app.component('example-component', ExampleComponent);
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
